06bcd3eb4fab783bb67c33be8cafa54917fc522f
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-08 19:16:38 +0600

    Project Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
+    extend: {
+      colors: {
+        firstColor: "#A02334",
+        secondColor: "#000",
+        secondColorLight: "#0c0c0c",
+        whiteColor: "#fff",
+        blackColor: "#000",
+        grayColor: "#c4c4c450",
+        paragraphColor: "#000000B3",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "24px",
+      },
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [],
 };
